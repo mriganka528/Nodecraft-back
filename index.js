c3f9ee80e7746d93a599efe8790d65e4c2f941d1
Mriganka Sarma <139118018+mriganka528@users.noreply.github.com>
2024-01-06 18:05:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ app.use(cors())
 app.use(express.json())
 
 // Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 app.use('/', (req, res) => {
   res.json({"message": "App deployed successfully"})
 })
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
 
 
 app.listen(port, () => {
