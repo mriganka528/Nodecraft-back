d6e01900a0322c98cc8ae74f69a654baba643923
Mriganka Sarma <139118018+mriganka528@users.noreply.github.com>
2024-01-06 15:25:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(express.json())
 
 // Available Routes
 
-app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
+app.use('/api/auth', require('./routes/auth'))
 
 
 app.listen(port, () => {
