59a14b8a1b814e08668d679f8a270340d4db3fc4
Mriganka Sarma <139118018+mriganka528@users.noreply.github.com>
2024-01-06 13:43:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,8 @@ const port = 9000;
 app.use(express.json());
 app.use(cors());
 //All Routes
-app.get('/', (req, res) => {
-  res.json({message:"Hello,myself Mriganka"})
-})
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/note', require('./routes/notes'));
-
 
 app.listen(port, () => {
   console.log(`NoteCraft listening on https://notecraft-back.vercel.app/`)
